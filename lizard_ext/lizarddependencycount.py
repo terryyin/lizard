e4dc118d460992c4211c0694ee12d3debcae1263
jason <jxieeducation@live.ca>
2014-04-19 21:31:06 -0700

    dependency #include basic support

--- lizard_ext/lizarddependencycount.py
+++ lizard_ext/lizarddependencycount.py
@@ -9,7 +9,8 @@ class LizardExtension(object):  # pylint: disable=R0903
     FUNCTION_INFO_PART = "dependency_count"
 
     def __call__(self, tokens, reader):
-        next_word_is_a_dependency = False
+        dependency_type = {'import': 1, '#include': 2}
+        next_word_is_a_dependency = 0
         import_list = []
         for token in tokens:
             if not hasattr(reader.context.current_function,
@@ -17,10 +18,17 @@ class LizardExtension(object):  # pylint: disable=R0903
                 reader.context.current_function.dependency_count = 0
             # this accounts for java, c, c++ and python's import
             if token == "import" or token == "#include":
-                next_word_is_a_dependency = True
-            elif next_word_is_a_dependency:
+                next_word_is_a_dependency = dependency_type[token]
+            elif next_word_is_a_dependency == dependency_type['import']:
                 import_list += [token]
-                next_word_is_a_dependency = False
+                next_word_is_a_dependency = 0
+            elif next_word_is_a_dependency == dependency_type['#include']:
+                #gets rid of the <> or "" as well as the .h
+                if len(token) >= 4:
+                    import_list += [token[1:len(token) - 3]]
+                else:
+                    import_list += [token[1:len(token) - 1]]
+                next_word_is_a_dependency = 0
 
             if token in import_list:
                 reader.context.current_function.dependency_count += 1
