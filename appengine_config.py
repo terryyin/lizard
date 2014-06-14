049d54e7aa9d24e36b8a5a41c02b5292cee2f6c4
Terry Yin <terry@odd-e.com>
2014-06-14 10:51:36 +0800

    move the site to master

---
+++ appengine_config.py
@@ -0,0 +1,7 @@
+"""`appengine_config` gets loaded when starting a new application instance."""
+import sys
+import os.path
+# add `lib` subdirectory to `sys.path`, so our `main` module can load
+# third-party libraries.
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib/local'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
