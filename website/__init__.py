3d02a86d63f6fb3167c98815c732541c0be8bf6d
Terry Yin <terry@odd-e.com>
2014-06-14 14:44:12 +0800

    using angular

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,19 @@
 import webapp2
+import json
+from json import JSONEncoder
+class FileInfoEncoder(JSONEncoder):
+        def default(self, o):
+                    return o.__dict__
+from lizard import analyze_file
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+class LizardAnalyzer(webapp2.RequestHandler):
+    def post(self):
+        dictionary = json.loads(self.request.body)
+        file_info = analyze_file.analyze_source_code("a.c", dictionary.get("code", ""))
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(file_info, cls=FileInfoEncoder))
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/analyze', LizardAnalyzer),
 ], debug=True)
 
---
+++ website/static/controllers.js
@@ -0,0 +1,29 @@
+(function() {
+  var app = angular.module('lizardApp', []);
+
+  app.controller('PhoneListCtrl', function ($scope) {
+  });
+
+  app.controller('SourceFormController', ['$scope', '$http', function ($scope, $http) {
+    $scope.source = {};
+    $scope.submit = function () {
+      $scope.progress = "analysing...";
+      $scope.result = {
+        function_list: [
+          {
+            name: "abc",
+            nloc: 10
+          }
+        ]
+      };
+
+      $http.post("/analyze", $scope.source).success(function(data) {
+        $scope.progress = "Code analyzed successfully.";
+        $scope.result = data;
+      }).error(function() {
+        $scope.progress = "Error connecting the analysing server.";
+      });
+   };
+  }]);
+
+})();
