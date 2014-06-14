f24a81c8910394851c48c4899d1d963bff7ff0c1
Terry Yin <terry@odd-e.com>
2014-06-14 10:35:58 +0800

    update

--- appengine_config.py
+++ appengine_config.py
@@ -3,5 +3,5 @@ import sys
 import os.path
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib/local'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
