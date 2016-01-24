aa9cf6478eebe0e8c68a44a8fa07990491e58cf8
Terry Yin <terry@odd-e.com>
2016-01-24 16:28:11 +0800

    windows exec

---
+++ lizard_langs.py
@@ -0,0 +1,15 @@
+from lizard import lizard_main
+import languages
+import sys
+
+def lizard_for_more_languages(args):
+    lans = [languages.JavaReader,
+        languages.JavaScriptReader,
+        languages.PythonReader,
+        languages.ObjCReader,
+        languages.TTCNReader,
+        languages.SwiftReader]
+    lizard_main(args, lans)
+
+if __name__ == "__main__":
+    lizard_for_more_languages(sys.argv)
