5ef2d33f6347a8ffae7f60dc13a7ab8414774735
Terry Yin <terry@odd-e.com>
2016-06-05 11:57:12 +0800

    scala

--- lizard_languages/scala.py
+++ lizard_languages/scala.py
@@ -1,8 +1,8 @@
 '''
 Language parser for Scala
 '''
-from .code_reader import CodeReader, CodeStateMachine
-from .clike import CCppCommentsMixin, CLikeReader
+from .clike import CLikeReader
+from .swift import SwiftStates
 __author__ = 'David Baum'
 
 
@@ -19,38 +19,9 @@ class ScalaReader(CLikeReader):
         self.parallel_states = [ScalaStates(context)]
 
 
-class ScalaStates(CodeStateMachine):  # pylint: disable=R0903
+class ScalaStates(SwiftStates):  # pylint: disable=R0903
     def _state_global(self, token):
         if token == 'def':
             self._state = self._function_name
         if token == 'main':
             self._function_name(token)
-
-    def _function_name(self, token):
-        self.context.start_new_function(token)
-        self._state = self._expect_function_dec
-
-    def _expect_function_dec(self, token):
-        if token == '(':
-            self._state = self._function_dec
-
-    def _function_dec(self, token):
-        if token == ')':
-            self._state = self._expect_function_impl
-        else:
-            self.context.parameter(token)
-
-    def _expect_function_impl(self, token):
-        if token == '{':
-            self._state = self._function_impl
-            self._state(token)
-
-    @CodeStateMachine.read_inside_brackets_then("{}")
-    def _function_impl(self, _):
-        self._state = self._state_global
-        self.context.end_of_function()
-
-    @CodeStateMachine.read_inside_brackets_then("{}")
-    def _protocol(self, end_token):
-        if end_token == "}":
-            self._state = self._state_global
