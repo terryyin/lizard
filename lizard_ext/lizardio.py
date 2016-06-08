6cf871365b207dca2a147b6724418b713f416e92
Mehrdad Mehraban <mehraban.mehrdad@gmail.com>
2016-06-08 17:12:51 +0200

    adding general_fan_out extension

--- lizard_ext/lizardio.py
+++ lizard_ext/lizardio.py
@@ -1,26 +1,52 @@
 '''
 Fan in and Fan out (again)
 '''
+from collections import Counter
 from .extension_base import ExtensionBase
+
+
+def get_all_indices(value, token_list):
+    indices = []
+    idx = -1
+    while True:
+        try:
+            idx = token_list.index(value, idx + 1)
+            indices.append(idx)
+        except ValueError:
+            break
+    return indices
 
 
 class LizardExtension(ExtensionBase):
     '''
+        Runs the Lizard Extension for the calculation of structural
+        fan-in and fan-out of every procedures in the source code.
+
+        Explanation of terms:
+        'ExtensionBase': Base class for all lizard extensions
+
+        Calculates:
+                'fan_in':                     Number of structural fan-in
+                'fan_out':		              Number of structural fan-out
+                                              in the local scope
+                'general_fan_out'             Number of structural fan-out
+                                              in the global scope
     '''
+
     FUNCTION_INFO = {
-            "fan_in":  {
-                "caption": " fan_in ",
-                "average_caption": " avg_fan_in ",
-                "regression": True},
-            "fan_out": {
-                "caption": " fan_out ",
-                "average_caption": " avg_fan_out ",
-                "regression": True},
-            "general_fan_out": {
-                "caption": " general_fan_out ",
-                "average_caption": " general_fan_out ",
-                "regression": True}
-            }
+        "fan_in": {
+            "caption": " fan_in ",
+            "average_caption": " avg_fan_in ",
+            "regression": True},
+        "fan_out": {
+            "caption": " fan_out ",
+            "average_caption": " avg_fan_out ",
+            "regression": True},
+        "general_fan_out": {
+            "caption": " general_fan_out ",
+            "average_caption": " general_fan_out ",
+            "regression": True}
+    }
 
     def __init__(self, context=None):
         self.all_methods = {}
@@ -28,23 +54,33 @@ class LizardExtension(ExtensionBase):
 
     def _state_global(self, token):
         if not hasattr(self.context.current_function, 'tokens'):
-            self.context.current_function.tokens = set()
-        self.context.current_function.tokens.add(token)
+            self.context.current_function.tokens = list()
+        self.context.current_function.tokens.append(token)
 
     def reduce(self, fileinfo):
         new_funcs = {f.unqualified_name: f for f in fileinfo.function_list}
         self.all_methods.update(new_funcs)
         self._add_to_fan_outs(new_funcs.keys())
-        self._add_to_general_fan_out(new_funcs)
+        self._add_to_general_fan_out()
         self._add_to_fan_ins(fileinfo.function_list)
 
     def _add_to_fan_outs(self, keys):
         for other_func in self.all_methods.values():
-            other_func.fan_out += len(other_func.tokens.intersection(keys))
+            intersect = Counter(keys) & Counter(other_func.tokens)
+            other_func.fan_out += len(list(intersect.elements()))
 
-    def _add_to_general_fan_out(self, new_funcs):
+    def _add_to_general_fan_out(self):
+        structures = set(['if', 'else', 'elif', 'for', 'foreach', 'while',
+                          'do', 'try', 'catch', 'switch', 'finally',
+                          'except', 'with'])
+        punctuations = set(['(', ')', '{', '}'])
         for other_func in self.all_methods.values():
-            other_func.general_fan_out += len(other_func.tokens.intersection(new_funcs.keys()))
+            bracket_indexes = get_all_indices('(', other_func.tokens)
+            for idx in bracket_indexes[1:]:
+                # Suggestion ?
+                if not other_func.tokens[idx - 1] in (structures and
+                                                      punctuations):
+                    other_func.general_fan_out += 1
 
     def _add_to_fan_ins(self, new_funcs):
         for func in new_funcs:
