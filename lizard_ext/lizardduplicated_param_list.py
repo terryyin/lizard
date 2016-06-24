c603f7f2ce8eb76b57c18eb17ab37c5a26c88d86
Terry Yin <terry@odd-e.com>
2016-06-24 22:06:31 +0800

    format

--- lizard_ext/lizardduplicated_param_list.py
+++ lizard_ext/lizardduplicated_param_list.py
@@ -26,17 +26,23 @@ class LizardExtension(ExtensionBase):
         super(LizardExtension, self).__init__(context)
 
     def reduce(self, fileinfo):
-        fl = [f for f in fileinfo.function_list if len(f.parameters) >= DEFAULT_MIN_PARAM_COUNT]
-        self.all_count.update(self._parameters(f) for f in fl)
-        self.all_count_per_file.update(set(self._parameters(f) for f in fl))
-        for f in fileinfo.function_list:
-            f.parameter_list_duplicates = 0
-            f.parameter_list_duplicated_in_files = 0
+        flt = [
+                f for f in fileinfo.function_list
+                if len(f.parameters) >= DEFAULT_MIN_PARAM_COUNT
+             ]
+        self.all_count.update(self._parameters(f) for f in flt)
+        self.all_count_per_file.update(set(self._parameters(f) for f in flt))
+        for flist in fileinfo.function_list:
+            flist.parameter_list_duplicates = 0
+            flist.parameter_list_duplicated_in_files = 0
 
     def reduce_again(self, fileinfo):
-        for f in fileinfo.function_list:
-            f.parameter_list_duplicates = self.all_count[self._parameters(f)]
-            f.parameter_list_duplicated_in_files = self.all_count_per_file[self._parameters(f)]
+        for flist in fileinfo.function_list:
+            flist.parameter_list_duplicates = self.all_count[
+                self._parameters(flist)]
+            flist.parameter_list_duplicated_in_files = \
+                self.all_count_per_file[self._parameters(flist)]
 
-    def _parameters(self, fi):
-        return ",".join(fi.parameters)
+    @staticmethod
+    def _parameters(finfo):
+        return ",".join(finfo.parameters)
