52990492099ca46951acfe3c44dca69cd5d0eb83
Terry Yin <terry.yinzhe@gmail.com>
2017-08-20 07:15:04 +0800

    remove website code

--- appengine_config.py
+++
@@ -1,7 +0,0 @@
-"""`appengine_config` gets loaded when starting a new application instance."""
-import sys
-import os.path
-# add `lib` subdirectory to `sys.path`, so our `main` module can load
-# third-party libraries.
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib/local'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
