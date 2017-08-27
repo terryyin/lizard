3b5b14ae9b65ef76e6c73e03aa53429437a0e25a
Terry Yin <terry.yinzhe@gmail.com>
2017-08-27 22:06:12 +0800

    compexity tags

---
+++ lizard_ext/lizardcomplextags.py
@@ -0,0 +1,18 @@
+'''
+This is an extension of lizard. It add a list of language keywords
+that adding the complexity and the line numbers of the keywords appear.
+'''
+
+
+class LizardExtension(object):  # pylint: disable=R0903
+
+    # pylint: disable=W0221
+    def __call__(self, tokens, reader):
+        context = reader.context
+        conditions = reader.conditions
+        for token in tokens:
+            yield token
+            if not hasattr(context.current_function, 'complex_tags'):
+                context.current_function.complex_tags = []
+            if token in conditions:
+                context.current_function.complex_tags.append([token, context.current_line])
---
+++ test/test_extensions/testComplexityTags.py
@@ -0,0 +1,27 @@
+import unittest
+from ..testHelpers import get_cpp_function_list_with_extnesion
+from lizard_ext.lizardcomplextags import LizardExtension as ComplexTags
+
+class TestNonStrictExtension(unittest.TestCase):
+
+    def test_empty_function(self):
+        result = get_cpp_function_list_with_extnesion('''
+        int fun(){}
+        ''', ComplexTags())
+        self.assertEqual([], result[0].complex_tags)
+
+    def test_function_with_one_condition(self):
+        result = get_cpp_function_list_with_extnesion('''
+        int fun(){if (a==b) c;}
+        ''', ComplexTags())
+        self.assertEqual([['if', 2]], result[0].complex_tags)
+
+    def test_multiple_lines(self):
+        result = get_cpp_function_list_with_extnesion('''
+        int fun(){
+            if (a==b)
+            c;
+            }
+        ''', ComplexTags())
+        self.assertEqual([['if', 3]], result[0].complex_tags)
+
