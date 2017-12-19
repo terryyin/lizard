2e168f39e1f7c596ab510eaeb063a0ec01beda5b
unknown <yalechen@yalechen-PC2.tencent.com>
2017-12-19 11:31:59 +0800

    change csv output.
    Signed-off-by: unknown <yalechen@yalechen-PC2.tencent.com>

--- lizard_ext/csvoutput.py
+++ lizard_ext/csvoutput.py
@@ -19,26 +19,27 @@ due to the nature of CSV outputs. The differences are:
 
 def csv_output(result, verbose):
     if verbose:
-        print("NLOC,CCN,token,PARAM,length,location,file,function,start,end")
+        print("NLOC,CCN,token,PARAM,length,location,file,function,long_name,start,end")
 
     for source_file in result:
         if source_file:
             for source_function in source_file.function_list:
                 if source_function:
-                    print('{},{},{},{},{},"{}","{}","{}",{},{}'.format(
+                    print('{},{},{},{},{},"{}","{}","{}","{}",{},{}'.format(
                         source_function.nloc,
                         source_function.cyclomatic_complexity,
                         source_function.token_count,
                         len(source_function.parameters),
                         source_function.end_line - source_function.start_line,
                         "{}@{}-{}@{}".format(
-                            source_function.name,
+                            source_function.name.replace("\"","'"),
                             source_function.start_line,
                             source_function.end_line,
                             source_file.filename
                         ),
                         source_file.filename,
-                        source_function.name,
+                        source_function.name.replace("\"","'"),
+                        source_function.long_name.replace("\"","'"),
                         source_function.start_line,
                         source_function.end_line
                     ))
