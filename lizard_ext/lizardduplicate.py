5deb80fe2492efe9aef95ffb90e3de10f4c9b983
Terry Yin <terry.yinzhe@gmail.com>
2018-05-14 06:08:01 -0500

    refactoring

--- lizard_ext/lizardduplicate.py
+++ lizard_ext/lizardduplicate.py
@@ -10,20 +10,24 @@ class Duplicate(object):
         self.start_line = start_line
         self.end_line = end_line
 
+
 class LizardExtension(ExtensionBase):
 
     def __init__(self, context=None):
         self.duplicates = []
         self.saved_sequences = []
+        self.cnt = 31
         super(LizardExtension, self).__init__(context)
 
     def __call__(self, tokens, reader):
         for token in tokens:
             self.saved_sequences.append([])
-            cnt = 31
-            for s in self.saved_sequences[-cnt:]:
+            for s in self.saved_sequences[-self.cnt:]:
                 s.append(token)
-            for p in (self.saved_sequences[:-cnt]):
-                if ''.join(self.saved_sequences[-cnt]) == ''.join(p):
-                    self.duplicates=[[Duplicate(1, 6), 1]]
+            for p in self._duplicates():
+                self.duplicates=[[Duplicate(1, 6), 1]]
             yield token
+
+    def _duplicates(self):
+        return [p for p in (self.saved_sequences[:-self.cnt])
+            if ''.join(self.saved_sequences[-self.cnt]) == ''.join(p)]
