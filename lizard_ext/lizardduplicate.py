472ad390d2ba010b814f67a10f031559ad3efdc3
Terry Yin <terry.yinzhe@gmail.com>
2018-05-14 08:35:35 +0800

    make it pass

--- lizard_ext/lizardduplicate.py
+++ lizard_ext/lizardduplicate.py
@@ -5,6 +5,10 @@ from collections import Counter
 from .extension_base import ExtensionBase
 
 
+class Duplicate(object):
+    def __init__(self, start_line):
+        self.start_line = start_line
+
 class LizardExtension(ExtensionBase):
 
     def __init__(self, context=None):
@@ -13,5 +17,5 @@ class LizardExtension(ExtensionBase):
 
     def __call__(self, tokens, reader):
         for token in tokens:
-            self.duplicates =[[1, 1]]
+            self.duplicates =[[Duplicate(1), 1]]
             yield token
