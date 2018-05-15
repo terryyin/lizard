fd3664fdec9c5e002f72afdd64253b39c8e2006f
Terry Yin <terry.yinzhe@gmail.com>
2018-05-15 06:03:37 -0500

    make it apss

--- lizard_ext/lizardduplicate.py
+++ lizard_ext/lizardduplicate.py
@@ -14,10 +14,11 @@ class CodeSnippet(object):
 class Sequence(object):
     def __init__(self, start_line):
         self.hash = ''
-        self.start_line = start_line
+        self.start_line = self.end_line = start_line
 
-    def append(self, token):
+    def append(self, token, current_line):
         self.hash += token
+        self.end_line = current_line
 
     def __eq__(self, other):
         return self.hash == other.hash
@@ -34,19 +35,21 @@ class LizardExtension(ExtensionBase):
         super(LizardExtension, self).__init__(context)
 
     def __call__(self, tokens, reader):
-        continuous = False
+        continuous = None
         for token in tokens:
             self.saved_sequences.append(Sequence(reader.context.current_line))
             for s in self.saved_sequences:
-                s.append(token)
+                s.append(token, reader.context.current_line)
             if len(self.saved_sequences) > self.SAMPLE_SIZE:
                 s = self.saved_sequences.popleft()
                 for p in self.saved_hash[s.hash]:
                     if not continuous:
-                        self.duplicates.append([CodeSnippet(p.start_line, p.start_line + 5), CodeSnippet(s.start_line, s.start_line + 5)])
-                        continuous = True
+                        continuous = [CodeSnippet(p.start_line, p.end_line), CodeSnippet(s.start_line, s.start_line + 5)]
+                        self.duplicates.append(continuous)
+                    continuous[0].end_line = p.end_line
+                    continuous[1].end_line = s.end_line
                 if not self.saved_hash[s.hash]:
-                    continuous = False
+                    continuous = None
                 self.saved_hash[s.hash].append(s)
 
             yield token
