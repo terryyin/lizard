99165002511ee9aa228670b4c8a71afbba1e9a0f
Terry Yin <terry.yinzhe@gmail.com>
2018-05-16 21:26:11 -0500

    refactoring

--- lizard_ext/lizardduplicate.py
+++ lizard_ext/lizardduplicate.py
@@ -41,6 +41,9 @@ class LizardExtension(ExtensionBase):
             self.find_duplicates(s)
 
             yield token
+        end_seq = Sequence(0)
+        end_seq.hash = -1
+        self.find_duplicates(end_seq)
 
     def _push_and_pop_current_sample_queue(self, token, current_line):
         self.saved_sequences.append(Sequence(current_line))
@@ -50,11 +53,12 @@ class LizardExtension(ExtensionBase):
 
     def find_duplicates(self, seq):
         if not self.saved_hash[seq.hash]:
-            self.active_dups = []
+            if self.active_dups:
+                self.duplicates.append(self.active_dups)
+                self.active_dups = []
         for p in self.saved_hash[seq.hash]:
             if not self.active_dups:
                 self.active_dups = [CodeSnippet(p.start_line), CodeSnippet(seq.start_line)]
-                self.duplicates.append(self.active_dups)
             self.active_dups[0].end_line = p.end_line
             self.active_dups[1].end_line = seq.end_line
         self.saved_hash[seq.hash].append(seq)
