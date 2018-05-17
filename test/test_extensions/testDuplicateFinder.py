314bead118b097700882e29ddb693c7101d234f0
Terry Yin <terry.yinzhe@gmail.com>
2018-05-16 22:04:39 -0500

    refactoring test

--- test/test_extensions/testDuplicateFinder.py
+++ test/test_extensions/testDuplicateFinder.py
@@ -3,10 +3,13 @@ from lizard_ext.lizardduplicate import DuplicateFinder
 
 class TestDuplicateFinder(unittest.TestCase):
 
-    def test_simple_duplicate(self):
+    def find_in(self, nodes):
         dups = []
         finder = DuplicateFinder(dups.append)
-        for t in [1, 1, -1]:
+        for t in nodes + [-1]:
             finder.find_duplicates(t, t)
-        self.assertEqual([[[1], [1]]], dups)
+        return dups
+
+    def test_simple_duplicate(self):
+        self.assertEqual([[[1], [1]]], self.find_in([1,1]))
 
