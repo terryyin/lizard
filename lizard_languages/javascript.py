e0fedeec2139979ba2531c893ab1fc68bba5c76c
Terry Yin <terry.yinzhe@gmail.com>
2019-12-28 20:27:53 +0800

    jsx

--- lizard_languages/javascript.py
+++ lizard_languages/javascript.py
@@ -18,32 +18,71 @@ class JavaScriptReader(CodeReader, CCppCommentsMixin):
     @staticmethod
     @js_style_regex_expression
     def generate_tokens(source_code, addition='', token_class=None):
+        raw_openning = r"\<(\w+)\s*(\/?)\>"
+        raw_closing = r"\<\/(\w+)\>"
         addition = addition +\
-                   r"|(?:\$\w+)" + \
-                   r"|(?:\<\w+\>)" + \
-                   r"|(?:\<\/\w+\>)" + \
-                   r"|`.*?`"
+                      r"|(?:\$\w+)" + \
+                      r"|(?:"+raw_openning + r")" + \
+                      r"|(?:"+raw_closing + r")" + \
+                      r"|(?:\<\/\w+\>)" + \
+                      r"|`.*?`"
 
-        xml_openning = re.compile(r"\<(\w+)\>")
-        xml_closing = re.compile(r"\<\/(\w+)\>")
-        saved = ''
-        openning = None
+        xml_openning = re.compile(raw_openning)
+        xml_closing = re.compile(raw_closing)
+
+        class JSTokenizer:
+            def __init__(self):
+                self.xml = None
+                self.openning = None
+
+            def __call__(self, token):
+                if self.xml:
+                    for tk in self.xml(token):
+                        yield tk
+                    if self.xml.ended:
+                        self.xml = None
+                    return
+                mch = xml_openning.match(token)
+                if not self.openning and mch:
+                    if mch[2] != '/':
+                        self.openning = mch[1]
+                        self.xml = XMLTokenizer(mch[1])
+                        return
+                yield token
+
+        class XMLTokenizer:
+            def __init__(self, tag):
+                self.tag = tag
+                self.depth = 1
+                self.saved = '<'+tag+'>'
+                self.ended = False
+
+            def __call__(self, token):
+                if token == "}":
+                    self.saved = token
+                    return
+                mch = xml_openning.match(token)
+                if mch and mch[1] == self.tag:
+                    self.depth += 1
+                mch = xml_closing.match(token)
+                if mch and mch[1] == self.tag:
+                    self.depth -= 1
+                    if self.depth == 0:
+                        yield self.saved + token
+                        self.saved = ''
+                        self.ended = True
+                        return
+                if self.saved:
+                    self.saved += token
+                    if token == '{':
+                        yield self.saved
+                        self.saved = ""
+
+        js_tokenizer = JSTokenizer()
         for token in CodeReader.generate_tokens(
                 source_code, addition, token_class):
-            mch = xml_openning.match(token)
-            if not openning and mch:
-                openning = mch[1]
-                saved = token
-                continue
-            mch = xml_closing.match(token)
-            if mch and mch[1] == openning:
-                yield saved + token
-                saved = ''
-                continue
-            if saved:
-                saved += token
-            else:
-                yield token
+            for tk in js_tokenizer(token):
+                yield tk
 
     def __init__(self, context):
         super(JavaScriptReader, self).__init__(context)
--- test/test_languages/testJSX.py
+++ test/test_languages/testJSX.py
@@ -12,14 +12,23 @@ class Test_tokenizing_JSX(unittest.TestCase):
         tokens = list(JavaScriptReader.generate_tokens(source))
         self.assertEqual(expect, tokens)
 
+    def test_simple_standalone(self):
+        self.check_tokens(['<abc />'], '<abc />')
+
     def test_simple_open_closing(self):
         self.check_tokens(['<abc></abc>'], '<abc></abc>')
 
     def test_open_closing_with_content(self):
-        self.check_tokens(['(', '<abc>xxx</abc>', ')'], '(<abc>xxx</abc>)')
+        self.check_tokens(['(', '<abc>xxx  +yyy</abc>', ')'], '(<abc>xxx  +yyy</abc>)')
 
     def test_nested(self):
         self.check_tokens(['(', '<abc><b>xxx</b></abc>', ')'], '(<abc><b>xxx</b></abc>)')
+
+    def test_nested_save_tag(self):
+        self.check_tokens(['(', '<b><b>xxx</b></b>', ')'], '(<b><b>xxx</b></b>)')
+
+    def test_with_embeded_code(self):
+        self.check_tokens(['<abc>{', '}</abc>'], '<abc>{}</abc>')
 
 class Test_parser_for_JavaScript_X(unittest.TestCase):
 
