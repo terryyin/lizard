c0fd05b627a3d2afb3c410aa0e7ce517560358e8
Patric Theune <dutch.edge@gmail.com>
2020-08-12 17:23:02 +0200

    Revert "Reverting to a build that almost worked for all versions"
    This reverts commit 3d14e7a2ce5791995deed809642c7e2fa494b5b3.

--- build.sh
+++ build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-echo Create Virtual Environment
-python3 -m venv venv
-
-echo Activate Virtual Environment
-source venv/bin/activate
-
-# Why is development mode necessary ?
-pip install -e .
-
-echo Prepare requirements within the Virtual Environments
-venv/bin/pip install -r dev_requirements.txt
+echo Setup virtual environment
+python -m venv venv
 
 echo Show the environment
 pip freeze
@@ -19,7 +10,3 @@ echo Install setup.py
 python setup.py build install
 
 make
-
-echo Deactivate Virtual Environment
-deactivate
-
