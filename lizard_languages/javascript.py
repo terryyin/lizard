c3cd418ce204dff6ddf223096d6a2aec2e7b5b51
Terry Yin <terry.yinzhe@gmail.com>
2025-01-31 17:30:46 +0800

    remove dup

--- lizard_languages/javascript.py
+++ lizard_languages/javascript.py
@@ -15,18 +15,6 @@ class JavaScriptReader(TypeScriptReader):
     ext = ['js']
     language_names = ['javascript', 'js']
 
-    @staticmethod
-    @js_style_regex_expression
-    def generate_tokens(source_code, addition='', token_class=None):
-        addition = addition +\
-            r"|(?:\$\w+)" + \
-            r"|`.*?`"
-        js_tokenizer = JSTokenizer()
-        for token in CodeReader.generate_tokens(
-                source_code, addition, token_class):
-            for tok in js_tokenizer(token):
-                yield tok
-
     def __init__(self, context):
         super(JavaScriptReader, self).__init__(context)
         self.parallel_states = [JavaScriptStyleLanguageStates(context)]
