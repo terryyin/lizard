542c5a682640446579aa1c811121db2c5d3e277c
Terry Yin <terry@odd-e.com>
2026-08-12 18:15:16 +0800

    refactor(csvoutput): simplify CSV output logic and enhance extension handling
    Refactored the CSV output function to improve clarity and maintainability by extracting the logic for collecting extension columns into a separate helper function. Updated the output formatting to streamline the process of generating CSV headers and rows, ensuring proper handling of extensions with multiple FUNCTION_INFO fields. Adjusted tests to reflect these changes and maintain coverage for various scenarios.

--- lizard_ext/csvoutput.py
+++ lizard_ext/csvoutput.py
@@ -17,39 +17,53 @@ due to the nature of CSV outputs. The differences are:
 """
 
 
+def _extension_columns(extensions):
+    """Collect FUNCTION_INFO field names and captions, in extension order."""
+    variables = []
+    captions = []
+    for extension in extensions:
+        if not hasattr(extension, "FUNCTION_INFO"):
+            continue
+        for name, info in extension.FUNCTION_INFO.items():
+            variables.append(name)
+            captions.append(info.get("caption", "No_caption"))
+    return variables, captions
+
+
 def csv_output(result, options):
     result = result.result
-
-    extension_variables = []
-    extension_captions = []
-    for extension in options.extensions:
-        if extension.__class__.__name__ == 'LizardExtension' and \
-            hasattr(extension, 'FUNCTION_INFO'):
-            for extension_variable in extension.FUNCTION_INFO:
-                extension_variables.append(extension_variable)
-                var_extension = extension.FUNCTION_INFO[extension_variable]
-                extension_caption = var_extension['caption'] \
-                    if 'caption' in var_extension else 'No_caption'
-                extension_captions.append(extension_caption)
+    extension_variables, extension_captions = _extension_columns(
+        options.extensions)
 
     if options.verbose:
-        extension_caption = ""
-        for caption in extension_captions:
-            extension_caption = f"{extension_caption},{caption}"
-        print(f"NLOC,CCN,token,PARAM,length,location,file,function," +
-              f"long_name,start,end{extension_caption}")
+        print(",".join([
+            "NLOC", "CCN", "token", "PARAM", "length", "location", "file",
+            "function", "long_name", "start", "end",
+        ] + extension_captions))
 
     for source_file in result:
-        if source_file:
-            for source_function in source_file.function_list:
-                if source_function:
-                    extension_string = ''
-                    for variable in extension_variables:
-                        extension_string = f"{extension_string},{source_function.__getattribute__(variable)}"
-                    print(f"{source_function.nloc},{source_function.cyclomatic_complexity},"
-                          f"{source_function.token_count},{len(source_function.parameters)},"
-                          f'{source_function.length},"{source_function.name.replace(chr(34), chr(39))}@'
-                          f'{source_function.start_line}-{source_function.end_line}@{source_file.filename}",'
-                          f'"{source_file.filename}","{source_function.name.replace(chr(34), chr(39))}",'
-                          f'"{source_function.long_name.replace(chr(34), chr(39))}",'
-                          f"{source_function.start_line},{source_function.end_line}{extension_string}")
+        if not source_file:
+            continue
+        for source_function in source_file.function_list:
+            if not source_function:
+                continue
+            name = source_function.name.replace('"', "'")
+            long_name = source_function.long_name.replace('"', "'")
+            fields = [
+                source_function.nloc,
+                source_function.cyclomatic_complexity,
+                source_function.token_count,
+                len(source_function.parameters),
+                source_function.length,
+                f'"{name}@{source_function.start_line}-'
+                f'{source_function.end_line}@{source_file.filename}"',
+                f'"{source_file.filename}"',
+                f'"{name}"',
+                f'"{long_name}"',
+                source_function.start_line,
+                source_function.end_line,
+            ]
+            fields.extend(
+                getattr(source_function, variable)
+                for variable in extension_variables)
+            print(",".join(str(field) for field in fields))
--- test/testOutputCSV.py
+++ test/testOutputCSV.py
@@ -1,137 +1,98 @@
-from unittest.mock import Mock, patch
-import unittest
+from unittest.mock import Mock
 import sys
 from lizard_ext import csv_output
+from lizard_ext.lizardio import LizardExtension as IoExtension
 from test.helper_stream import StreamStdoutTestCase
-from lizard import parse_args, print_and_save_modules, FunctionInfo, FileInformation,\
-    get_extensions, OutputScheme, AllResult
+from lizard import FunctionInfo, FileInformation, AllResult
 
 
 class TestCSVOutput(StreamStdoutTestCase):
 
     def setUp(self):
         StreamStdoutTestCase.setUp(self)
-        self.option = parse_args("app")
         self.foo = FunctionInfo("foo", 'FILENAME', 100)
         self.fileSummary = FileInformation("FILENAME", 123, [self.foo])
-        self.extensions = get_extensions([])
-        self.scheme = OutputScheme(self.extensions)
+
+    def _options(self, extensions=None, verbose=False):
+        options = Mock()
+        options.verbose = verbose
+        options.extensions = extensions or []
+        return options
+
+    def _results_with_attrs(self, **attrs):
+        results = AllResult([self.fileSummary])
+        function = results.result[0].function_list[0]
+        for name, value in attrs.items():
+            setattr(function, name, value)
+        return results
 
     def test_csv_header(self):
-        options_mock = Mock()
-        options_mock.verbose = True
-        options_mock.extensions = []
-        csv_output(AllResult([self.fileSummary]), options_mock)
-        self.assertRegex(sys.stdout.stream,
-                                 r"NLOC,CCN,token,PARAM,length,location,file,function,long_name,start,end")
+        csv_output(AllResult([self.fileSummary]), self._options(verbose=True))
+        self.assertRegex(
+            sys.stdout.stream,
+            r"NLOC,CCN,token,PARAM,length,location,file,function,"
+            r"long_name,start,end")
 
     def test_csv_header_with_extension(self):
-        options_mock = Mock()
-        options_mock.verbose = True
-        extension_mock = Mock()
-        extension_mock.__class__.__name__ = 'LizardExtension'
-        extension_mock.FUNCTION_INFO = {"exit_count": {"caption": "exits"}}
-        options_mock.extensions = [extension_mock]
-        results = AllResult([self.fileSummary])
-        results.result[0].function_list[0].exit_count = 1
-        csv_output(results, options_mock)
-        self.assertRegex(sys.stdout.stream,
-                                 r"NLOC,CCN,token,PARAM,length,location,file,function,long_name,start,end,exits")
+        extension = Mock()
+        extension.FUNCTION_INFO = {"exit_count": {"caption": "exits"}}
+        csv_output(
+            self._results_with_attrs(exit_count=1),
+            self._options([extension], verbose=True))
+        self.assertRegex(
+            sys.stdout.stream,
+            r"NLOC,CCN,token,PARAM,length,location,file,function,"
+            r"long_name,start,end,exits")
 
     def test_csv_no_header(self):
-        options_mock = Mock()
-        options_mock.verbose = False
-        options_mock.extensions = []
-        csv_output(AllResult([self.fileSummary]), options_mock)
+        csv_output(AllResult([self.fileSummary]), self._options())
         self.assertEqual(
-            '1,1,1,0,1,"foo@100-100@FILENAME","FILENAME","foo","foo",100,100',
+            '1,1,1,0,1,"foo@100-100@FILENAME","FILENAME","foo","foo",'
+            '100,100',
             sys.stdout.stream.splitlines()[0]
         )
 
     def test_csv_no_header_with_extension(self):
-        options_mock = Mock()
-        options_mock.verbose = False
-        options_mock.extensions = []
-        extension_mock = Mock()
-        extension_mock.__class__.__name__ = 'LizardExtension'
-        extension_mock.FUNCTION_INFO = {"exit_count": {"caption": "exits"}}
-        options_mock.extensions = [extension_mock]
-
-        results = AllResult([self.fileSummary])
-        results.result[0].function_list[0].exit_count = 1
-        csv_output(results, options_mock)
-
+        extension = Mock()
+        extension.FUNCTION_INFO = {"exit_count": {"caption": "exits"}}
+        csv_output(
+            self._results_with_attrs(exit_count=1),
+            self._options([extension]))
         self.assertEqual(
-            '1,1,1,0,1,"foo@100-100@FILENAME","FILENAME","foo","foo",100,100,1',
+            '1,1,1,0,1,"foo@100-100@FILENAME","FILENAME","foo","foo",'
+            '100,100,1',
             sys.stdout.stream.splitlines()[0]
         )
 
     def test_csv_header_with_multi_field_extension(self):
-        options_mock = Mock()
-        options_mock.verbose = True
-        extension_mock = Mock()
-        extension_mock.__class__.__name__ = 'LizardExtension'
-        extension_mock.FUNCTION_INFO = {
-            "metric_one": {"caption": " metric_one "},
-            "metric_two": {"caption": " metric_two "},
-        }
-        options_mock.extensions = [extension_mock]
-        results = AllResult([self.fileSummary])
-        results.result[0].function_list[0].metric_one = 12.5
-        results.result[0].function_list[0].metric_two = 99.0
-        csv_output(results, options_mock)
+        csv_output(
+            self._results_with_attrs(
+                fan_in=1, fan_out=2, general_fan_out=3),
+            self._options([IoExtension()], verbose=True))
         self.assertRegex(
             sys.stdout.stream,
             r"NLOC,CCN,token,PARAM,length,location,file,function,"
-            r"long_name,start,end, metric_one , metric_two ")
+            r"long_name,start,end, fan_in , fan_out , general_fan_out ")
 
     def test_csv_row_with_multi_field_extension(self):
-        options_mock = Mock()
-        options_mock.verbose = False
-        extension_mock = Mock()
-        extension_mock.__class__.__name__ = 'LizardExtension'
-        extension_mock.FUNCTION_INFO = {
-            "metric_one": {"caption": " metric_one "},
-            "metric_two": {"caption": " metric_two "},
-        }
-        options_mock.extensions = [extension_mock]
-        results = AllResult([self.fileSummary])
-        results.result[0].function_list[0].metric_one = 12.5
-        results.result[0].function_list[0].metric_two = 99.0
-        csv_output(results, options_mock)
+        csv_output(
+            self._results_with_attrs(
+                fan_in=1, fan_out=2, general_fan_out=3),
+            self._options([IoExtension()]))
         self.assertEqual(
             '1,1,1,0,1,"foo@100-100@FILENAME","FILENAME","foo","foo",'
-            '100,100,12.5,99.0',
+            '100,100,1,2,3',
             sys.stdout.stream.splitlines()[0]
         )
 
     def test_print_fileinfo(self):
-        options_mock = Mock()
-        options_mock.verbose = True
-        options_mock.extensions = []
-
         self.foo.end_line = 100
         self.foo.cyclomatic_complexity = 16
         file_stat = FileInformation("FILENAME", 1, [self.foo])
 
-        csv_output(AllResult([file_stat]), options_mock)
+        csv_output(AllResult([file_stat]), self._options(verbose=True))
         self.assertEqual(
-            '1,16,1,0,1,"foo@100-100@FILENAME","FILENAME","foo","foo",100,100',
-            sys.stdout.stream.splitlines()[1]
-        )
-
-    def test_print_extension(self):
-        options_mock = Mock()
-        options_mock.verbose = True
-        options_mock.extensions = []
-
-        self.foo.end_line = 100
-        self.foo.cyclomatic_complexity = 16
-        file_stat = FileInformation("FILENAME", 1, [self.foo])
-
-        csv_output(AllResult([file_stat]), options_mock)
-
-        self.assertEqual(
-            '1,16,1,0,1,"foo@100-100@FILENAME","FILENAME","foo","foo",100,100',
+            '1,16,1,0,1,"foo@100-100@FILENAME","FILENAME","foo","foo",'
+            '100,100',
             sys.stdout.stream.splitlines()[1]
         )
